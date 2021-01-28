f76d1561039c81ea7a73fa5ee4e3d770b3ba640e
Russkazkin <ruslan@skazkin.su>
2021-01-28 07:49:01 +0300

    Add ProductsOverview screen

--- lib/screens/products_overview.dart
+++ lib/screens/products_overview.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+
+import '../data/products.dart';
 import '../models/product.dart';
 
 class ProductsOverview extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, index) => Container(),
+        padding: const EdgeInsets.all(10),
+        itemCount: dummyProduct.length,
+      ),
+    );
   }
 }
