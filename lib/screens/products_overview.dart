3619621f21230cb61d0da5560f7494bd98162f7a
Russkazkin <ruslan@skazkin.su>
2021-01-28 07:36:20 +0300

    Add dummyProducts data

--- lib/screens/products_overview.dart
+++ lib/screens/products_overview.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import '../models/product.dart';
 
 class ProductsOverview extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Container();
