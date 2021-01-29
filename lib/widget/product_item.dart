d206ddaf8e1cf405fb1e387b0deca54fb313cbee
Russkazkin <ruslan@skazkin.su>
2021-01-29 07:50:07 +0300

    Add ProductItem footer

--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -9,6 +9,20 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl));
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        leading: IconButton(icon: Icon(Icons.favorite_border), onPressed: () {},),
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(icon: Icon(Icons.shopping_cart_rounded), onPressed: () {},),
+      ),
+    );
   }
 }
