568f37f43a5e908859809efc080a97f7aa0ea000
Russkazkin <ruslan@skazkin.su>
2021-02-04 07:50:28 +0300

    Add provider to the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/product_detail.dart';
 import 'screens/products_overview.dart';
+import 'providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverview(),
+        routes: {
+          ProductDetail.route: (context) => ProductDetail(),
+        },
       ),
-      home: ProductsOverview(),
-      routes: {
-        ProductDetail.route: (context) => ProductDetail(),
-      },
     );
   }
 }
