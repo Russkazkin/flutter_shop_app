648d8eaacae84ca381f3aaae9a33eb881ff5e84d
Russkazkin <ruslan@skazkin.su>
2021-02-08 07:05:55 +0300

    Add findById method for Products provider

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetail extends StatelessWidget {
   static const route = '/product-detail';
@@ -6,9 +9,13 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context).settings.arguments as String;
+    final product = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(id);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(product.title),
       ),
     );
   }
