d675278f353605bda38ca274cd2ddc9f16bfaff4
Russkazkin <ruslan@skazkin.su>
2021-02-11 07:42:19 +0300

    Add favorites filter

--- lib/screens/products_overview.dart
+++ lib/screens/products_overview.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widget/products_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductsOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
         actions: [
           PopupMenuButton(
-            onSelected: (int selectedValue) {
-              print(selectedValue);
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
             },
             itemBuilder: (_) => [
               PopupMenuItem(
                 child: Text(
                   'Only Favorites',
                 ),
-                value: 0,
+                value: FilterOptions.Favorites,
               ),
               PopupMenuItem(
                 child: Text(
                   'Show All',
                 ),
-                value: 1,
+                value: FilterOptions.All,
               ),
             ],
             icon: Icon(
