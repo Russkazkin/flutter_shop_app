43b7d793b6762a382be91dbc9eb1c6640f63fbf1
Russkazkin <ruslan@skazkin.su>
2021-02-25 07:39:25 +0300

    Add orders provider

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -48,4 +48,9 @@ class Cart with ChangeNotifier {
     _items.remove(productId);
     notifyListeners();
   }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
 }
--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+
+import '../models/CartItem.dart';
+import '../models/OrderItem.dart';
 
 class Orders with ChangeNotifier {
-  
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [...this._orders];
+  }
+
+  void addOrder(List<CartItem> cartProducts, double total) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: Uuid().v1(),
+        amount: total,
+        dateTime: DateTime.now(),
+        products: cartProducts,
+      ),
+    );
+    notifyListeners();
+  }
 }
