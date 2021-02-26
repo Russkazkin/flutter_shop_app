a96138e6572be0a6c93fcb96fe6042ec88d34736
Russkazkin <ruslan@skazkin.su>
2021-02-26 07:56:51 +0300

    Add ORDER NOW handler for the cart screen

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' as CartProvider;
+import '../providers/orders.dart';
 import '../widget/cart_item.dart';
 
 class Cart extends StatelessWidget {
@@ -40,7 +41,13 @@ class Cart extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                     child: Text('ORDER NOW'),
                     textColor: Theme.of(context).primaryColor,
                   ),
