e487733f56e20574b63b78e845310f2b550c7bf1
Russkazkin <ruslan@skazkin.su>
2021-03-04 07:48:50 +0300

    Add a SnackBar with cart add confirmaiton and UNDO button

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -20,25 +20,29 @@ class Cart with ChangeNotifier {
     return total;
   }
 
-  void addItem(String productId,
-      double price,
-      String title,) {
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
     if (_items.containsKey(productId)) {
-      _items.update(productId, (existingItem) =>
-          CartItem(id: existingItem.id,
-              title: existingItem.title,
-              quantity: existingItem.quantity + 1,
-              price: existingItem.price),);
+      _items.update(
+        productId,
+        (existingItem) => CartItem(
+            id: existingItem.id,
+            title: existingItem.title,
+            quantity: existingItem.quantity + 1,
+            price: existingItem.price),
+      );
     } else {
       _items.putIfAbsent(
         productId,
-            () =>
-            CartItem(
-              id: DateTime.now().toString(),
-              title: title,
-              quantity: 1,
-              price: price,
-            ),
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          quantity: 1,
+          price: price,
+        ),
       );
     }
     notifyListeners();
@@ -46,6 +50,24 @@ class Cart with ChangeNotifier {
 
   void removeItem(String productId) {
     _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) return;
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (item) => CartItem(
+          id: item.id,
+          title: item.title,
+          quantity: item.quantity - 1,
+          price: item.price,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 
--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -52,6 +52,13 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart_rounded),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Added item to cart!', textAlign: TextAlign.center,),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(label: 'UNDO', onPressed: () => cart.removeSingleItem(product.id),),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
