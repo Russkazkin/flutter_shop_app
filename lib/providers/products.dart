97f8140775642482a0f76f931e1d84822eedabe4
Russkazkin <ruslan@skazkin.su>
2021-04-09 07:00:22 +0300

    Add post http request to save new product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'product.dart';
 import '../data/products.dart';
@@ -19,6 +22,14 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    final url = Uri.parse('https://flutter-shop-1ef5f-default-rtdb.firebaseio.com/products.json');
+    http.post(url, body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,
+      'isFavorite': product.isFavorite,
+    }));
     final newProduct = Product(
       id: DateTime.now().toString(),
       title: product.title,
