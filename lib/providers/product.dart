d20d05f9234a34d05f5d11bd8847ce21b66fa9be
Russkazkin <ruslan@skazkin.su>
2021-04-20 07:26:08 +0300

    Save favorite to the db

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../models/http_exception.dart';
 
 class Product with ChangeNotifier{
   final String id;
@@ -17,8 +21,24 @@ class Product with ChangeNotifier{
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final productUrl = Uri.parse(
+        'https://flutter-shop-1ef5f-default-rtdb.firebaseio.com/products/$id.json');
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final response = await http.patch(productUrl, body: jsonEncode({
+        'isFavorite': isFavorite
+      }));
+      if (response.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+        throw HttpException('Could not change favorite status.');
+      }
+    } catch (_) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
   }
 }
