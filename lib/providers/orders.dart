0d7c061919c7e2b6de398ae305385f6f1b7a5157
Russkazkin <ruslan@skazkin.su>
2021-04-21 07:17:45 +0300

    Add http request to addOrder method

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,23 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/CartItem.dart';
 import '../models/OrderItem.dart';
 
 class Orders with ChangeNotifier {
+  final url = Uri.parse(
+      'https://flutter-shop-1ef5f-default-rtdb.firebaseio.com/orders.json');
+
   List<OrderItem> _orders = [];
 
   List<OrderItem> get orders {
     return [...this._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final timeStamp = DateTime.now();
+    final response = await http.post(url, body: json.encode({
+      'amount': total,
+      'dateTime': timeStamp.toIso8601String(),
+      'products': cartProducts.map((cartProduct) => {
+        'id': cartProduct.id,
+        'title': cartProduct.title,
+        'quantity': cartProduct.quantity,
+        'price': cartProduct.price,
+      }).toList(),
+    }),);
     _orders.insert(
       0,
       OrderItem(
-        id: Uuid().v1(),
+        id: json.decode(response.body)['name'],
         amount: total,
-        dateTime: DateTime.now(),
+        dateTime: timeStamp,
         products: cartProducts,
       ),
     );
