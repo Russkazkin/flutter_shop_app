9b5cfe7e13b03873ca1c39e281874662935ad08a
Russkazkin <ruslan@skazkin.su>
2021-05-04 07:30:07 +0300

    Add login functionallity

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,10 +9,7 @@ class Auth with ChangeNotifier {
   DateTime _expireDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-            env['FIREBASE_KEY']);
+  Future<void> _authenticate(String email, String password, Uri url) async {
     final response = await http.post(
       url,
       body: json.encode(
@@ -24,5 +21,20 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+            env['FIREBASE_KEY']);
+    return _authenticate(email, password, url);
+
+  }
+
+  Future<void> login(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+            env['FIREBASE_KEY']);
+    return _authenticate(email, password, url);
   }
 }
