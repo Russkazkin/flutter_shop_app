c3a69bb163206ec65dfa4496367e80782a5ee555
Russkazkin <ruslan@skazkin.su>
2021-05-05 07:19:54 +0300

    Throw exception for auth acitons errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,23 +4,32 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expireDate;
   String _userId;
 
   Future<void> _authenticate(String email, String password, Uri url) async {
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final data = json.decode(response.body);
+      if (data['error'] != null) {
+        throw HttpException(data['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
